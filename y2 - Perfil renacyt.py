dc2693de5137807461f2ffbdaf6ece2a25c56fe2
NahumFGz <ngutierrez2507@gmail.com>
2023-12-02 23:58:35 -0500

    ejecucion por partes

--- y2 - Perfil renacyt.py
+++ y2 - Perfil renacyt.py
@@ -62,7 +62,7 @@ def format_json(dir_path):
 
 def clean_dir(dir_name):
     try:
-        files = get_files(os.path.join(os.getcwd(),'outputs','jsons',dir_name), '.')
+        files = get_files(os.path.join(os.getcwd(),'outputs','jsons',dir_name), '.json')
         for file in files:
             os.remove(file)
         print('Se eliminaron los archivos de la carpeta {}'.format(dir_name))
@@ -78,7 +78,6 @@ def clean_logs():
     except:
         pass
 
-
 ########################
 ## C. Ejecución
 ########################
@@ -89,8 +88,8 @@ PRINT_VIEW = False
 
 # 2. Eliminar todos los archivos de la carpeta
 clean_dir(os.path.join(os.getcwd(),'originals','renacyt','perfil','0_json'))
-clean_dir(os.path.join(os.getcwd(),'originals','renacyt','perfil','1_datos_investigador'))
-clean_dir(os.path.join(os.getcwd(),'originals','renacyt','perfil','2_datos_investigador_format'))
+# clean_dir(os.path.join(os.getcwd(),'originals','renacyt','perfil','1_datos_investigador'))
+# clean_dir(os.path.join(os.getcwd(),'originals','renacyt','perfil','2_datos_investigador_format'))
 
 
 ######################
@@ -110,70 +109,83 @@ df_renacyt = df_renacyt.astype(str)
 # 2. Seleccionar la columna ctiVitae y crear otra columna con el url + ctiVitae
 df_renacyt['url'] = 'https://ctivitae.concytec.gob.pe/buscador-ui/#/ficha/ficha-renacyt?idInvestigador=' + df_renacyt['ctiVitae']
 df_renacyt = df_renacyt[['ctiVitae','orcid','url']]
+print('Registros originales: ' + str(df_renacyt.shape[0]))
 
-# df_renacyt = df_renacyt.head(3)
-df_renacyt
+# 3. Leer el parquet de ejecuciones
+df_ejecuciones_base = pd.read_parquet(os.path.join(os.getcwd(),'originals','ejecuciones_renacyt.parquet'))
 
+# 4. Obtener el maximo valor de i
+df_ejecuciones_base['i'] = df_ejecuciones_base['i'].astype(int)
+max_i = df_ejecuciones_base['i'].max()
+
+if pd.isna(max_i):
+    max_i = 0
+else:
+    max_i = max_i + 1
+print('Valor de max_i: ' + str(max_i))
+
+# 5. A df_renacyt['ctiVitae'] filtrar todos los df_ejecuciones_base['cti_vitae']
+df_renacyt = df_renacyt[~df_renacyt['ctiVitae'].isin(df_ejecuciones_base['cti_vitae'])]
+print('Registros filtrados: ' + str(df_renacyt.shape[0]))
+
+df_renacyt = df_renacyt.head(5)
+print('Registros a ejecutar: ' + str(df_renacyt.shape[0]))
 
 ######################
 ## E. Ejecutar
 ######################
 
 # 1. Iterar sobre las urls
-i = 0
+i = max_i
 for cti_vitae,orcid, url in df_renacyt.values.tolist(): 
-    try:
-        # A. Obtener el driver
-        driver, server, proxy = get_chrome_driver(chromedriver_path=CHROMEDRIVER_PATH, print_view=PRINT_VIEW, headless=HEADLESS)
+    # A. Obtener el driver
+    driver, server, proxy = get_chrome_driver(chromedriver_path=CHROMEDRIVER_PATH, print_view=PRINT_VIEW, headless=HEADLESS)
 
-        # B. Iniciar har
-        start_har(proxy, 'renacyt_profile')
-        
-        # C. Ingresar a la URL y esperar a que la tabla específica esté presente en el DOM
-        driver.get(url)
-        wait = WebDriverWait(driver, 10)
-        table_locator = (By.CSS_SELECTOR, 'table.table.table-sm')
-        table = wait.until(EC.presence_of_element_located(table_locator))
-        print(f'{i}. {url}')
+    # B. Iniciar har
+    start_har(proxy, 'renacyt_profile')
+    
+    # C. Ingresar a la URL y esperar a que la tabla específica esté presente en el DOM
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+    table_locator = (By.CSS_SELECTOR, 'table.table.table-sm')
+    table = wait.until(EC.presence_of_element_located(table_locator))
+    print(f'{i}. {url}')
 
-        # D. Obtener el har
-        time.sleep(4)
-        har = proxy.har
+    # D. Obtener el har
+    time.sleep(4)
+    har = proxy.har
 
-        # E. Guardar json original
-        j = 0
-        k = 0
-        for entrie in har.get('log').get('entries'):
+    # E. Guardar json original
+    j = 0
+    k = 0
+    for entrie in har.get('log').get('entries'):
 
-            # JSON original
-            data_list = str(entrie)
-            with open(os.path.join(os.getcwd(),'originals','renacyt','perfil','0_json', str(i) + 'x' + str(j) + '.json'), 'w') as json_file:
+        # JSON original
+        data_list = str(entrie)
+        with open(os.path.join(os.getcwd(),'originals','renacyt','perfil','0_json', str(i) + 'x' + str(j) + '.json'), 'w') as json_file:
+            json.dump(data_list, json_file, indent=4)
+            j += 1
+
+        # Datos del investigador
+        string_content = entrie.get('response').get('content').get('text')
+        if '"formacionesAcademicas"' in str(string_content):
+            data_list = json.loads(string_content)
+            with open(os.path.join(os.getcwd(),'originals','renacyt','perfil','1_datos_investigador'
+                                    , str(i) + 'x' + str(k) 
+                                    + '_' + str(cti_vitae)
+                                    + '_' + str(orcid) +'_.json'), 'w') as json_file:
                 json.dump(data_list, json_file, indent=4)
-                j += 1
+                k += 1
 
-            # Datos del investigador
-            string_content = entrie.get('response').get('content').get('text')
-            if '"formacionesAcademicas"' in str(string_content):
-                data_list = json.loads(string_content)
-                with open(os.path.join(os.getcwd(),'originals','renacyt','perfil','1_datos_investigador'
-                                       , str(i) + 'x' + str(k) 
-                                       + '_' + str(cti_vitae)
-                                       + '_' + str(orcid) +'_.json'), 'w') as json_file:
-                    json.dump(data_list, json_file, indent=4)
-                    k += 1
+    # F. Finalizar driver, server, proxy y eliminar los logs
+    stop_chrome_driver(driver, server, proxy)
+    clean_dir(os.path.join(os.getcwd(),'originals','renacyt','perfil','0_json'))
+    
+    # G. Actualizar el registro de ejecuciones_renacyt
+    df_ejecuciones_base = pd.read_parquet(os.path.join(os.getcwd(),'originals','ejecuciones_renacyt.parquet'))
+    df_ejecuciones_actual = pd.concat([df_ejecuciones_base, pd.DataFrame({'i': [i], 'cti_vitae': [cti_vitae], 'orcid': [orcid]})], axis=0)
+    df_ejecuciones_actual.to_parquet(os.path.join(os.getcwd(),'originals','ejecuciones_renacyt.parquet'), index=False)
 
-        # F. Finalizar driver, server, proxy y eliminar los logs
-        stop_chrome_driver(driver, server, proxy)
-        clean_dir(os.path.join(os.getcwd(),'originals','renacyt','perfil','0_json'))
-        #clean_logs()
-        
-        # G. Actualizar el contador
-        i += 1
-        print('')
+    # H. Actualizar el contador
+    i += 1
 
-    except:
-        pass
-
-
-# 2. Formatear los jsons
-format_json(os.path.join(os.getcwd(),'originals','renacyt','perfil','1_datos_investigador'))
